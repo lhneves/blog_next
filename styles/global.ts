9e0d331b99e904e2f865520c3539865f12cb5230
Luiz Doretto <luiz.n.doretto@gmail.com>
2021-09-30 20:22:01 -0300

    style(global): set global theme variables and style

--- styles/global.ts
+++ styles/global.ts
@@ -1,9 +1,59 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-    *{
-        padding: 0;
-        margin: 0;
-        box-sizing: border-box;
+   *,
+  *::after,
+  *::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    backface-visibility: hidden;
+    text-rendering: geometricprecision;
+    -webkit-font-smoothing: antialiased;
+    -moz-font-smoothing: antialiased;
+    font-family: 'Poppins', sans-serif;
+  }
+
+  :focus {
+    outline: none;
+  }
+
+  input::-ms-clear {
+    display: none;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: ${({ theme }) => theme.font.sizes.medium}
+  }
+
+  a {
+    color: ${({ theme }) => theme.colors.secondary};
+    text-decoration: none;
+    transition: opacity 300ms ease-in-out;
+    cursor: pointer;
+
+    &:hover {
+      opacity: .6;
     }
+  }
+
+  button {
+    background-color: transparent;
+    border: 0;
+    cursor: pointer;
+    outline: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+
 `;
+
+export const rem = (pixels: number) => `${pixels / 10}rem`;
--- styles/theme.ts
+++ styles/theme.ts
@@ -1,5 +1,25 @@
+import { rem } from './global';
+
 export const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: '#010101',
+    secondary: '#005b96',
+    white: '#fafafa',
+    lightGray: '#dddddd',
+    gray: '#999999',
+    darkDray: '#333333',
+  },
+  spacings: {
+    small: '1rem',
+    medium: '2rem',
+    large: '3rem',
+  },
+  font: {
+    sizes: {
+      small: rem(12),
+      medium: rem(18),
+      large: rem(20),
+      superLarge: rem(50),
+    },
   },
 };
