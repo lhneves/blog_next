064224d3a9daac1b71248803c22473d4a3047b21
Luiz Doretto <luiz.n.doretto@gmail.com>
2021-10-04 16:11:47 -0300

    feat(fetch): tranform markdown to html
    Install remark and transform markdown to html text.

--- src/data/posts/get-post.ts
+++ src/data/posts/get-post.ts
@@ -1,3 +1,4 @@
+import { markdownToHtml } from './../../utils/markdown-to-html';
 import { POSTS_URL } from '../../config/app-config';
 import { PostData } from '../../domain/posts/posts';
 import { fetchJson } from '../../utils/fetch-json';
@@ -7,5 +8,7 @@ export const getPost = async (slug: string | string[]): Promise<PostData[]> => {
   const slugString = Array.isArray(slug) ? slug[0] : slug;
   const url = `${POSTS_URL}?slug=${slugString}`;
   const jsonPost = await fetchJson<PostData[]>(url);
-  return jsonPost;
+  const content = await markdownToHtml(jsonPost[0].content);
+  const finalContent = { ...jsonPost[0], content };
+  return [finalContent];
 };
