e8b44e2112d801e4ad879e31bdb9d9ec70b25ab4
Luiz Doretto <luiz.n.doretto@gmail.com>
2021-10-06 19:04:29 -0300

    feat(date): create date and format date

---
+++ src/config/app-config.ts
@@ -0,0 +1,5 @@
+export const API_URL = 'https://mighty-bayou-02847.herokuapp.com';
+export const POSTS_URL = `${API_URL}/posts`;
+
+export const SITE_NAME = 'Blog Next';
+export const SITE_URL = '';
